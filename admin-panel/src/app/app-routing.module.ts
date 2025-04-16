5345c74cf4969b6f0007e258ea066f74e5ec7b2b
Vishal prajapat <96687108+vpbgkt@users.noreply.github.com>
2025-04-16 12:32:51 +0000

    Pending changes exported from your codespace

--- admin-panel/src/app/app-routing.module.ts
+++ admin-panel/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { QuestionListComponent } from './components/question-list/question-list.component';
 import { AddQuestionComponent } from './components/add-question/add-question.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'questions', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'questions', component: QuestionListComponent },
   { path: 'add-question', component: AddQuestionComponent },
 ];
