76b16a20e48fec67c3eb8fe552578d67f6a3cf1c
vishal prajapat <vpbgkt@gmail.com>
2025-04-18 19:14:30 +0530

    chore: removed unused submitController and route, cleaned up broken import

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,6 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/questions', require('./routes/questions'));
 app.use('/api/tests', require('./routes/tests'));
-app.use('/api/submit', require('./routes/submit'));
 app.use('/api/hierarchy', require('./routes/hierarchy'));
 app.use('/api/subjects', require('./routes/subject'));
 app.use('/api/topics', require('./routes/topic'));
